0ce405924903cda501af5aa28eb5d776f45e9edd
Souvik00076 <souvikbhattacharjee00076@gmail.com>
2025-04-06 13:36:39 +0000

    resolved snack.nvim error

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,22 @@
+return {
+  {
+    "craftzdog/solarized-osaka.nvim",
+    lazy = true,
+    priority = 1000,
+    opts = function()
+      return {
+        transparent = true,
+      }
+    end,
+  },
+}
+----
+--return {
+--  { "nyoom-engineering/oxocarbon.nvim" },
+--  {
+--    "LazyVim/LazyVim",
+--    opts = {
+--      colorscheme = "oxocarbon",
+--    },
+--  },
+--}
